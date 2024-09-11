93bc03515e24f489bf1bce21cc771fdea768dab9
Jesus Navas <jesusnavas2210@gmail.com>
2024-09-11 01:34:27 -0400

    [MOD] fixed response hub

--- backend/app/Enums/Provider.php
+++ backend/app/Enums/Provider.php
@@ -3,19 +3,18 @@
 namespace App\Enums;
 
 use App\Services\Providers\HotelLegs\HotelLegsAdapter;
+use App\Services\Providers\Speedia\SpeediaAdapter;
 
 class Provider
 {
     public const PROVIDERS = [
         '1' => self::HOTEL_LEGS,
-        '2' => self::TRAVEL_DOOR_X,
-        '3' => self::SPEEDIA,
+        '2' => self::SPEEDIA,
     ];
 
     public const PROVIDERS_CLASSES = [
         '1' => HotelLegsAdapter::class,
-        '2' => HotelLegsAdapter::class,
-        '3' => HotelLegsAdapter::class,
+        '2' => SpeediaAdapter::class,
     ];
 
     public const HOTEL_LEGS = [
@@ -23,13 +22,8 @@ class Provider
         'name' => 'Hotel Legs',
     ];
 
-    public const TRAVEL_DOOR_X = [
-        'hotelId' => 2,
-        'name' => 'TravelDoorX',
-    ];
-
     public const SPEEDIA = [
-        'hotelId' => 3,
+        'hotelId' => 2,
         'name' => 'Speedia',
     ];
 
