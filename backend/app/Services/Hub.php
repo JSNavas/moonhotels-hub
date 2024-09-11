30d9684bfd3cbb0f0db123ab2e9231df2a919fbe
Ivan Castillo <ivantelix@gmail.com>
2024-09-11 01:04:35 -0400

    design-patter: Added provider Speedia logic and refactor code HubResponse

--- backend/app/Services/Hub.php
+++ backend/app/Services/Hub.php
@@ -5,25 +5,30 @@ namespace App\Services;
 use App\Http\Requests\SearchRequest;
 use App\Http\Responses\HubResponse;
 use App\Services\Providers\HotelLegs\HotelLegsAdapter;
+use App\Services\Providers\Speedia\SpeediaAdapter;
 
 class Hub
 {
     private array $providers = [
         HotelLegsAdapter::class,
-        // Otros adaptadores
+        SpeediaAdapter::class,
     ];
 
     public function search(SearchRequest $request): HubResponse
     {
-        $responses = [];
+        $responses = collect([]);
         foreach ($this->providers as $providerClass) {
-
             $provider = app($providerClass);
             $providerRequest = $provider->translateRequest($request);
             $providerResponse = $provider->search($providerRequest);
-            $responses = $provider->translateResponse($providerResponse);
+            $responses->push($provider->translateResponse($providerResponse));
         }
 
-        return new HubResponse(collect($responses)->flatten(1));
+        // Aplanar las respuestas para eliminar el primer key 'rooms'
+        $flattenedResponses = $responses->flatMap(function ($response) {
+            return $response->rooms;
+        });
+
+        return new HubResponse($flattenedResponses);
     }
 }
---
+++ backend/app/Services/Providers/Speedia/SpeediaAdapter.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Services\Providers\Speedia;
+
+use App\Interfaces\ProviderInterface;
+use App\Http\Requests\SearchRequest;
+use App\Services\Providers\Speedia\SpeediaRequest;
+use App\Services\Providers\Speedia\SpeediaResponse;
+use App\Http\Requests\ProviderRequest;
+use App\Http\Responses\HubResponse;
+use App\Http\Responses\ProviderResponse;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use Illuminate\Support\Collection;
+
+class SpeediaAdapter implements ProviderInterface
+{
+    public function translateRequest(SearchRequest $request): SpeediaRequest
+    {
+        // Lógica para transformar la solicitud del HUB al formato de HotelLegs
+        return new SpeediaRequest(query: [
+            'id_hotel' => $request->hotelId,
+            'check_in' => $request->checkIn,
+            'check_out' => $request->checkOut,
+            'number_guests' => $request->numberOfGuests,
+            'number_rooms' => $request->numberOfRooms,
+            'currency' => $request->currency,
+        ]);
+    }
+
+    public function search(ProviderRequest $request): SpeediaResponse
+    {
+        // Realizar la petición a la API de HotelLegs y devolver la respuesta
+        // $client = new Client();
+        // $response = $client->request('GET', 'https://api.hotellegs.com/search', [
+        //     'query' => $request->toArray(),
+        // ]);
+
+        $data = $this->getData()
+                        ->when(
+                            !is_null($request->id_hotel),
+                            fn($q) => $q->where('id_hotel', '=', $request->id_hotel)
+                        )
+                        ->when(
+                            !is_null($request->check_in),
+                            fn($q) => $q->where('date', '>=', $request->check_in)
+                        )
+                        ->when(
+                            !is_null($request->check_out),
+                            fn($q) => $q->where('date', '<=', $request->check_out),
+                        )
+                        ->when(
+                            !is_null($request->number_guests),
+                            fn($q) => $q->where('num_person'. '>=', $request->number_guests)
+                        )
+                        ->when(
+                            !is_null($request->number_rooms),
+                            fn($q) => $q->where('num_rooms', '=', $request->number_rooms)
+                        )
+                        ->when(
+                            !is_null($request->currency),
+                            fn($q) => $q->where('currency', '=', $request->currency)
+                        );
+
+        return new SpeediaResponse($data->toArray());
+    }
+
+    public function translateResponse($response): HubResponse
+    {
+        // Lógica para transformar la respuesta de HotelLegs al formato del HUB
+        $results = collect($response->results);
+        $hubResponse = new HubResponse();
+
+        $hubResponse->rooms = $results->map(function ($result) {
+            return [
+                'roomId' => $result['num_rooms'],
+                'rates' => [
+                    [
+                        'mealPlanId' => $result['num_meal'],
+                        'isCancellable' => $result['can_cancel'],
+                        'price' => $result['cost'],
+                    ],
+                ],
+            ];
+        });
+        return $hubResponse;
+    }
+
+    private function getData(): Collection
+    {
+        return collect([
+            [
+                'id_hotel'          => 2,
+                'num_rooms'         => 1,
+                'num_meal'          => 1,
+                'can_cancel'        => false,
+                'cost'              => 135.48,
+                'numPerson'         => 2,
+                'date'              => Carbon::createFromFormat('Y-m-d', '2024-09-05'),
+                'currency'          => 'EUR'
+            ],
+            [
+                'id_hotel'          => 2,
+                'num_rooms'         => 2,
+                'num_meal'          => 1,
+                'can_cancel'        => true,
+                'cost'              => 145.00,
+                'numPerson'         => 4,
+                'date'              => Carbon::createFromFormat('Y-m-d', '2024-09-10'),
+                'currency'          => 'EUR'
+            ],
+            [
+                'id_hotel'          => 2,
+                'num_rooms'         => 3,
+                'num_meal'          => 1,
+                'can_cancel'        => false,
+                'cost'              => 155.25,
+                'numPerson'         => 5,
+                'date'              => Carbon::createFromFormat('Y-m-d', '2024-09-15'),
+                'currency'          => 'EUR'
+            ]
+        ]);
+    }
+}
